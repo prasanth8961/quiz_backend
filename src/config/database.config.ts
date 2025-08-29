dac74a9bd760c77e151d4727fac5b1353390e484
prasanth8961 <prasanthprs143@gmail.com>
2025-08-29 13:13:14 +0530

    Fix: bug in datasource config

--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -13,7 +13,7 @@ export const AppDataSource = new DataSource({
   host: process.env.DB_HOST,
   port: parseInt(process.env.DB_PORT || '3306', 10),
   username: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
+  password: process.env.DB_PASS,
   database: process.env.DB_NAME,
   entities: [Player, Quiz, Question, PlayerQuizScore, QuizQuestion],
   synchronize: false,
